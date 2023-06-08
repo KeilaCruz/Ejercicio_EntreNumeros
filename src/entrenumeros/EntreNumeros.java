c0c57f8e1fee164ffb4f15ecc46790fcb0fa1221
Keila <zS20019068@estudiantes.uv.mx>
2023-06-08 13:50:04 -0600

    Actualización imprimir rango de impares

--- src/entrenumeros/EntreNumeros.java
+++ src/entrenumeros/EntreNumeros.java
@@ -1,12 +1,14 @@
 package entrenumeros;
+
 import java.util.Scanner;
 
-public class EntreNumeros{
+public class EntreNumeros {
     /**
      * Método main toma dos numeros enteros de entrada
+     * 
      * @param args
      */
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Ingrese un numero: ");
         int numUno = input.nextInt();
@@ -14,18 +16,22 @@ public class EntreNumeros{
         int numDos = input.nextInt();
         imprimirNumeros(numUno, numDos);
     }
+
     /**
      * Metodo imprime los valores entre el numero uno y el numero dos
      * excluyendolos
+     * 
      * @param numUno
      * @param numDos
      */
-    public static void imprimirNumeros(int numUno, int numDos){
-        if(numUno >= numDos){
+    public static void imprimirNumeros(int numUno, int numDos) {
+        if (numUno >= numDos) {
             System.out.println("El num uno no es valido");
         }
-        for(int i=numUno+1; i<numDos; i++){
-            System.out.print(i + " ");
+        for (int i = numUno + 1; i < numDos; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
         }
     }
 }
